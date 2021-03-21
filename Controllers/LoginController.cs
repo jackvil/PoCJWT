9c82209ea4af1b53bbc60e65d5a966f87f57dd8a
José Andrés Clavijo <clavijonet@gmail.com>
2021-03-20 21:18:40 -0500

    Versión estable de la prueba de concepto

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Proof of concept JWT
+ * Sample for: https://enmilocalfunciona.io/construyendo-una-web-api-rest-segura-con-json-web-token-en-net-parte-ii/
+ */
+using PoCJWT.Models;
+using System.Net;
+using System.Threading;
+using System.Web.Http;
+
+namespace PoCJWT.Controllers
+{
+
+    [AllowAnonymous]
+    [RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpGet]
+        [Route("echouser")]
+        public IHttpActionResult EchoUser()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(LoginRequest login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            bool isCredentialValid = (login.Username.Equals("jackvil") && login.Password.Equals("123456"));
+            if (isCredentialValid)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.Username);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace PoCJWT.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        [HttpGet]
+       
+        public IHttpActionResult GetId(int id)
+        {
+            var customerFake = "jackvil";
+            return Ok(customerFake);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var users = new string[] { "jackvil", "test1", "test2" };
+            return Ok(users);
+        }
+    }
+}
